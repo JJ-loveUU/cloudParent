68117e12132087d43b2e8dac51b1a53c834009f5
JJ-loveUU <837430579@qq.com>
2020-11-01 18:54:23 +0800

    Update NotVeryUsefulAspect.java

--- common/src/main/java/commonAspect/aspect/NotVeryUsefulAspect.java
+++ common/src/main/java/commonAspect/aspect/NotVeryUsefulAspect.java
@@ -124,7 +124,7 @@ public class NotVeryUsefulAspect {
 
   public static void main(String[] args) {
     System.out.println(77777);
-    System.out.println(6666);
+    System.out.println(55555);
   }
 
 
